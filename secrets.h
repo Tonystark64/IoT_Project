7dd8439773fd1581287035340766581f25153375
Tonystark64 <1183694330@qq.com>
2022-11-17 20:01:47 +0800

    Add files via upload
    for security

--- secrets.h
+++ secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "ELS-AN00%4355%CloudClone"       // Wi-Fi account        
-#define SECRET_PASS "04088203"   // Wi-Fi passwd  
+#define SECRET_SSID "Your Wi-Fi Account"       // Wi-Fi account        
+#define SECRET_PASS "Your Wi-Fi Password"   // Wi-Fi passwd  
 
-#define SECRET_CH_ID 1940204       // channel No.
-#define SECRET_WRITE_APIKEY "V5SHHNK7T1D0Z6G5"    // channel write api key
+#define SECRET_CH_ID 1234567       // channel No.
+#define SECRET_WRITE_APIKEY "XXXXXXXXXXXX"    // channel write api key
